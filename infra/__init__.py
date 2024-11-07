8404a5863bfd86d0854b3360fd7085b5c2ca7775
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-11-07 11:21:37 -0300

    chore: Atualizado importações do modulo

--- infra/__init__.py
+++ infra/__init__.py
@@ -1,2 +1,7 @@
 from infra.entities import Bank, BankAccount, Transaction
 from infra.repository import BankRepository, BankAccountRepository, TransactionRepository
+from infra.configs import DBConnectionHandler, Base
+
+with DBConnectionHandler() as db:
+    engine = db.get_engine()
+    Base.metadata.create_all(engine)
---
+++ src/financial/__init__.py
@@ -0,0 +1,5 @@
+from src.financial.bank import BankModel
+from src.financial.account import AccountModel
+from src.financial.transaction import TransactionModel, TransactionsTypes
+from src.financial.database_handler import DatabaseHandler, BankDatabaseHandler, AccountDatabaseHandler, TransactionDatabaseHandler
+from src.financial.system import FinancialHandler
