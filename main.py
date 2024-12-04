f4b9aab8e87dc5b46d342d9d7a80b6c8f127ded9
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-03 21:32:54 -0300

    feat: Atualização em informações e variáveis erradas

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ user01 = UserModel(nickname="Marcos")
 
 acc01 = AccountModel(name="Acc01", description="TesteXX", created_by=user01.user_id) # 130
 acc02 = AccountModel(name="Acc02", description="Teste02", created_by=user01.user_id) # 40
-acc03 = AccountModel(name="Acc03", description="Teste03", created_by=user01.user_id) # 50
+acc03 = AccountModel(name="Acc03", description="Teste03", created_by=user01.user_id) # 70
 
-trasaction01 = TransactionModel(description="Add 10", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc01.account_id, created_by=user01.user_id)
-trasaction02 = TransactionModel(description="Add 10", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc02.account_id, created_by=user01.user_id)
-trasaction03 = TransactionModel(description="Add 10", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc03.account_id, created_by=user01.user_id)
-trasaction04 = TransactionModel(description="Add 10", amount=10, transaction_type=TransactionsTypes.WITHDRAW, destination=acc01.account_id, created_by=user01.user_id)
-trasaction05 = TransactionModel(description="Add 10", amount=20, transaction_type=TransactionsTypes.WITHDRAW, destination=acc02.account_id, created_by=user01.user_id)
-trasaction05 = TransactionModel(description="Add 10", amount=30, transaction_type=TransactionsTypes.WITHDRAW, destination=acc03.account_id, created_by=user01.user_id)
-trasaction06 = TransactionModel(description="Add 10", amount=40, transaction_type=TransactionsTypes.WITHDRAW, destination=acc01.account_id, origin=acc02.account_id, created_by=user01.user_id)
+trasaction01 = TransactionModel(description="Add 100 01", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc01.account_id, created_by=user01.user_id)
+trasaction02 = TransactionModel(description="Add 100 02", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc02.account_id, created_by=user01.user_id)
+trasaction03 = TransactionModel(description="Add 100 03", amount=100, transaction_type=TransactionsTypes.DEPOSIT, destination=acc03.account_id, created_by=user01.user_id)
+trasaction04 = TransactionModel(description="Rem 10 01", amount=10, transaction_type=TransactionsTypes.WITHDRAW, destination=acc01.account_id, created_by=user01.user_id)
+trasaction05 = TransactionModel(description="Rem 20 02", amount=20, transaction_type=TransactionsTypes.WITHDRAW, destination=acc02.account_id, created_by=user01.user_id)
+trasaction06 = TransactionModel(description="Rem 30 03", amount=30, transaction_type=TransactionsTypes.WITHDRAW, destination=acc03.account_id, created_by=user01.user_id)
+trasaction07 = TransactionModel(description="Trf 40 02 01", amount=40, transaction_type=TransactionsTypes.TRANSFER, destination=acc01.account_id, origin=acc02.account_id, created_by=user01.user_id)
 
 fh = FinancialHandler()
 
@@ -30,24 +30,10 @@ fh.add_transaction(trasaction03)
 fh.add_transaction(trasaction04)
 fh.add_transaction(trasaction05)
 fh.add_transaction(trasaction06)
+fh.add_transaction(trasaction07)
 
-
-trasaction_id = trasaction01.transaction_id
-
-# modificando a transação
-trasaction01.amount = Decimal("999.99")
-trasaction01.description = "Teste de transação 999.99"
-trasaction01.calculate = False
-trasaction01.status = True
-
-fh.update_transaction(transaction_id=trasaction_id, transaction=trasaction01)
-
-
-
-
-# DEU ZEBRA AQUI
-# for trs in fh.get_all_transactions():
-#     fh._execute_transaction(trs.transaction_id)
+for trs in fh.get_all_transactions():
+    fh._execute_transaction(trs.transaction_id)
 
 
 
