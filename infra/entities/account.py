159464cf1def08826d32e2d62fa8489eaa3a8468
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-18 21:22:49 -0300

    refactor: Modificado 'account' com argumentos coerentes

--- infra/entities/account.py
+++ infra/entities/account.py
@@ -7,14 +7,15 @@ from infra.configs import Base
 class Account(Base):
     __tablename__ = "accounts"
     
-    account_id = Column(String(64), primary_key=True, nullable=False)
+    id = Column(String(64), primary_key=True, nullable=False)
     name = Column(String(64), nullable=False)
     description = Column(String(256), nullable=False)
+    account_tag_id = Column(String(64), ForeignKey("accounts_tags.id"), nullable=False)
     balance = Column(Float, nullable=False)
     created_at = Column(DateTime, nullable=False)
-    created_by = Column(String(64), ForeignKey("users.user_id"), nullable=False)
+    user_id = Column(String(64), ForeignKey("users.id"), nullable=False)
     
-    def __repr__(self):
-        return (f"<Account(account_id='{self.account_id}', name='{self.name}', "
-                f"description='{self.description}', created_at='{self.created_at}', "
-                f"created_by='{self.created_by}')>")
+    # def __repr__(self):
+    #     return (f"<Account(account_id='{self.account_id}', name='{self.name}', "
+    #             f"description='{self.description}', created_at='{self.created_at}', "
+    #             f"created_by='{self.created_by}')>")
