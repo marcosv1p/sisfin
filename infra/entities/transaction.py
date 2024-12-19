806b64d4632085ea13cd275cde4f47fbf63780da
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-18 21:24:16 -0300

    refactor: Modificado entidades do banco de dados

--- infra/entities/transaction.py
+++ infra/entities/transaction.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, DateTime, ForeignKey, Float, Boolean
 
 from infra.configs import Base
@@ -8,20 +7,23 @@ from infra.configs import Base
 class Transaction(Base):
     __tablename__ = "transactions"
     
-    transaction_id = Column(String(64), primary_key=True, nullable=False)
-    date = Column(DateTime, default=datetime.now, nullable=False)
+    id = Column(String(64), primary_key=True, nullable=False)
+    date = Column(DateTime, nullable=False)
     description = Column(String(255), nullable=False)
-    amount = Column(Float, nullable=False) # Troquei de value para amount
+    amount = Column(Float, nullable=False)
     transaction_type = Column(String(64), nullable=False)
-    status = Column(Boolean, default=False, nullable=False)
-    calculate = Column(Boolean, default=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    created_by = Column(String(64), ForeignKey("users.user_id"), nullable=False)
-    origin = Column(String(64), ForeignKey("accounts.account_id"))
-    destination = Column(String(64), ForeignKey("accounts.account_id"), nullable=False)
+    paid = Column(Boolean, nullable=False)
+    ignore = Column(Boolean, nullable=False)
+    visible = Column(Boolean, nullable=False)
+    category_id = Column(String(64), ForeignKey("transactions_categories.id"), nullable=False)
+    tag_id = Column(String(64), ForeignKey("transactions_tags.id"), nullable=False)
+    account_id_origin = Column(String(64), ForeignKey("accounts.id"))
+    account_id_destination = Column(String(64), ForeignKey("accounts.id"), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user_id = Column(String(64), ForeignKey("users.id"), nullable=False)
     
-    def __repr__(self):
-        return (f"<Transaction(transaction_id='{self.transaction_id}', date='{self.date}', "
-                f"description='{self.description}', amount={self.amount}, transaction_type='{self.transaction_type}', "
-                f"status={self.status}, calculate='{self.calculate}', created_at='{self.created_at}', "
-                f"created_by='{self.created_by}', origin='{self.origin}', destination='{self.destination}')>")
+    # def __repr__(self):
+    #     return (f"<Transaction(transaction_id='{self.transaction_id}', date='{self.date}', "
+    #             f"description='{self.description}', amount={self.amount}, transaction_type='{self.transaction_type}', "
+    #             f"status={self.status}, calculate='{self.calculate}', created_at='{self.created_at}', "
+    #             f"created_by='{self.created_by}', origin='{self.origin}', destination='{self.destination}')>")
