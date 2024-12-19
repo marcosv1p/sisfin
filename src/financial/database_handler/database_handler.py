203035361c1fba1afe4c83301cb37413ec0d09cd
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-18 21:29:08 -0300

    delete: Descontinuado lidadores para o banco de dados

--- src/financial/database_handler/database_handler.py
+++
@@ -1,6 +0,0 @@
-from src.financial.database_handler import UserDatabaseHandler, AccountDatabaseHandler, TransactionDatabaseHandler
-
-class DatabaseHandler:
-    handler_user = UserDatabaseHandler
-    handler_account = AccountDatabaseHandler
-    handler_transaction = TransactionDatabaseHandler
