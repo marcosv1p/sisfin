427e0b7ce412b7a8930558aa3c0e3a7f39bdc0dc
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-22 14:57:09 -0300

    refactor: Removido TrasactionCategoryModel

--- src/financial/models/transaction_model.py
+++ src/financial/models/transaction_model.py
@@ -8,28 +8,6 @@ from pydantic import Field, BaseModel
 from src.financial.interfaces import DataInterface
 
 
-class TransactionCategoryModel(DataInterface):
-    # UUID da categoria
-    id: UUID = Field(default_factory=uuid4)
-    
-    # Nome da categoria
-    name: str
-    
-    # Data de criação da categoria
-    created_at: datetime = Field(default_factory=datetime.now)
-    
-    # UUID do usuario que criou que pertense a categoria
-    user_id: UUID
-    
-    def get_current_dict_data(self) -> dict:
-        return {
-            "id": str(self.id),
-            "name": self.name,
-            "created_at": self.created_at,
-            "user_id": str(self.user_id)
-        }
-
-
 class TransactionTypes(Enum):
     EXPENSE = "despesa"
     INCOME = "renda"
