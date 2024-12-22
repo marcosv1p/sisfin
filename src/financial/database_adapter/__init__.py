b49ee3351a90fb1fa2d9b0c043ce33b7419c54f4
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-22 14:35:35 -0300

    style: Adicionado nova importação

--- src/financial/database_adapter/__init__.py
+++ src/financial/database_adapter/__init__.py
@@ -2,3 +2,4 @@ from src.financial.database_adapter.user_db_adapter import UserDatabaseAdapter
 from src.financial.database_adapter.account_db_adapter import AccountDatabaseAdapter
 from src.financial.database_adapter.transaction_db_adapter import TransactionDatabaseAdapter
 from src.financial.database_adapter.account_tag_db_adapter import AccountTagDatabaseAdapter
+from src.financial.database_adapter.transaction_tag_db_adapter import TransactionTagDatabaseAdapter
