1fc340fbdf8954b66e2b9ddf530e35a3f152b262
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-22 14:41:49 -0300

    refactor: Movido TrasactionTagModel para modulo separado

--- src/financial/models/transaction_model.py
+++ src/financial/models/transaction_model.py
@@ -8,28 +8,6 @@ from pydantic import Field, BaseModel
 from src.financial.interfaces import DataInterface
 
 
-class TransactionTagModel(BaseModel):
-    # UUID da tag
-    id: UUID = Field(default_factory=uuid4)
-    
-    # Nome da tag
-    name: str
-    
-    # Data de criação da tag
-    created_at: datetime = Field(default_factory=datetime.now)
-    
-    # UUID do usuario que criou que pertense a tag
-    user_id: UUID
-    
-    def get_current_dict_data(self) -> dict:
-        return {
-            "id": str(self.id),
-            "name": self.name,
-            "created_at": self.created_at,
-            "user_id": str(self.user_id)
-        }
-
-
 class TransactionCategoryModel(DataInterface):
     # UUID da categoria
     id: UUID = Field(default_factory=uuid4)
