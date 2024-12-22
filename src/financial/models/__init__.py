8e1490c575d289c4c87416e56e93485ab4c831d0
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-22 14:14:31 -0300

    style: Modificado origem de importações

--- src/financial/models/__init__.py
+++ src/financial/models/__init__.py
@@ -1,3 +1,4 @@
 from src.financial.models.user_model import UserModel
-from src.financial.models.account_model import AccountModel, AccountTagModel
+from src.financial.models.account_model import AccountModel
+from src.financial.models.account_tag_model import AccountTagModel
 from src.financial.models.transaction_model import TransactionModel, TransactionCategoryModel, TransactionTagModel, TransactionTypes
