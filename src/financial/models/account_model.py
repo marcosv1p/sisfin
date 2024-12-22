ccf6ea01d0ac2c528ae9a1ceb1b13ead91e3261c
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-22 14:41:20 -0300

    refactor: Movido AccountTagModel para modulo separado

--- src/financial/models/account_model.py
+++ src/financial/models/account_model.py
@@ -5,28 +5,6 @@ from datetime import datetime
 from pydantic import Field, BaseModel
 
 
-class AccountTagModel(BaseModel):
-    # UUID da tag
-    id: UUID = Field(default_factory=uuid4)
-    
-    # Nome da tag
-    name: str
-    
-    # Data de criação da tag
-    created_at: datetime = Field(default_factory=datetime.now)
-    
-    # UUID do usuario que criou que pertense a tag
-    user_id: UUID
-    
-    def get_current_dict_data(self) -> dict:
-        return {
-            "id": self.id,
-            "name": self.name,
-            "created_at": self.created_at,
-            "user_id": self.user_id
-        }
-
-
 class AccountModel(BaseModel):
     # UUID da conta
     id: UUID = Field(default_factory=uuid4)
