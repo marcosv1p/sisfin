60e8725bd0260ca242d2cf92f4a3aba382509300
Marcos Vinícios Piva <117595005+marcosv1p@users.noreply.github.com>
2024-12-28 17:37:18 -0300

    feat: Criado novos erros personalizados para UserHandler

---
+++ src/financial/exceptions/handler_errors/user_handler_error.py
@@ -0,0 +1,50 @@
+from src.financial.exceptions.handler_errors.handler_error import HandlerError
+from src.financial.exceptions.code_errors import FinacialErrorGroup, FinacialErrorTag, FinacialErrorType
+
+
+class UserHandlerError(HandlerError):
+    def __init__(self,
+                error_tag:FinacialErrorTag=FinacialErrorTag.HANDLER,
+                error_group:FinacialErrorGroup=FinacialErrorGroup.USER,
+                error_type: FinacialErrorType = FinacialErrorType.INVALID_INPUT,
+                error_message: str = "Error generico em 'UserHandler'",
+                **kwargs):
+        super().__init__(
+            error_tag=error_tag,
+            error_group=error_group,
+            error_type=error_type,
+            error_message=error_message,
+            **kwargs
+        )
+
+
+class UnexpectedArgumentTypeError(UserHandlerError):
+    def __init__(self,
+                error_tag:FinacialErrorTag=FinacialErrorTag.HANDLER,
+                error_group:FinacialErrorGroup=FinacialErrorGroup.USER,
+                error_type: FinacialErrorType = FinacialErrorType.INVALID_INPUT,
+                error_message: str = "Tipo de argumento inesperado",
+                **kwargs):
+        super().__init__(
+            error_tag=error_tag,
+            error_group=error_group,
+            error_type=error_type,
+            error_message=error_message,
+            **kwargs
+        )
+
+
+class UnexpectedDatabaseTypeError(UnexpectedArgumentTypeError):
+    def __init__(self,
+                error_tag:FinacialErrorTag=FinacialErrorTag.HANDLER,
+                error_group:FinacialErrorGroup=FinacialErrorGroup.USER,
+                error_type: FinacialErrorType = FinacialErrorType.INVALID_INPUT,
+                error_message: str = "Tipo inesperado do argumento 'databese'",
+                **kwargs):
+        super().__init__(
+            error_tag=error_tag,
+            error_group=error_group,
+            error_type=error_type,
+            error_message=error_message,
+            **kwargs
+        )
